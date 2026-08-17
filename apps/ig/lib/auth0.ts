d8c01dfb2e30d2d877e22a7b0e09a1f426bc1cd5
aman <aman2@willy2912.com>
2026-08-17 19:39:56 +0530

    auth0 env

--- apps/ig/lib/auth0.ts
+++ apps/ig/lib/auth0.ts
@@ -1,8 +1,8 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
 export const auth0 = new Auth0Client({
-  clientId: "gaTFpEk4xShkCJL83QlVFZXGbMKyogzA",
-  clientSecret: "DICdPIhyCCZFYLM0cCYEhkj3Bn_0t2cT6MBk8k-llR9xDT3pta6g95RlxBBVl0hj",
- 
-  secret: "THIS_IS_A_LONG_RANDOM_STRING_AT_LEAST_32_CHARS"
+ domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  secret: process.env.AUTH0_SECRET,
 });
